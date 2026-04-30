8ac0538c974b6bc505a7f76b3db9d6aca320a6f9
cive202 <civecodes@gmail.com>
2026-04-30 12:29:49 +0000

    cleaning_pipeline

--- chapter1/data_cleaning/pipeline.py
+++ chapter1/data_cleaning/pipeline.py
@@ -50,13 +50,14 @@ def validate_pipeline(df, required_columns):
         ==> Remove records if necessary
 '''
 
+
 def handle_missing_values(df):
 
     for col in df.select_datatype(include='number').columns:
-        df[col].fillna(df[col].median(),inplace=True)
+        df[col].fillna(df[col].median(), inplace=True)
 
     for col in df.select_datatype(include='category').columns:
-        df[col].fillna("Unkown",inplace=True)
+        df[col].fillna("Unkown", inplace=True)
 
     return df
 
@@ -65,13 +66,16 @@ def handle_missing_values(df):
     Removing Duplicate Records:
 '''
 
+
 def remove_duplicates(df):
     df.drop_duplicated(inplace=True)
     return df
 
+
 '''
     DataType Standardization
 '''
+
 
 def standardize_datatype(df):
 
@@ -83,6 +87,7 @@ def standardize_datatype(df):
 
     return df
 
+
 '''
     cleaning categorical Data
         ==> It may contain inconsistent values due to
@@ -91,47 +96,53 @@ def standardize_datatype(df):
             ==> abbreviations (like male ->m , female ->f)
 '''
 
+
 def cleaning_categorical_data(df):
     if "gender" in df.columns:
         df["gender"] = df["gender"].str.lower().lower()
         df["gender"] = df["gender"].replace({
-            "m" : "male",
-            "f" : "female",
-            })
+            "m": "male",
+            "f": "female",
+        })
     return df
+
 
 '''
     handling outliers
 '''
 
-def handle_outliers(df,column):
+
+def handle_outliers(df, column):
     q1 = df[column].quantile(0.25)
     q3 = df[column].quantile(0.75)
     iqr = q3 - q1
 
     lower_limit = q1 - 1.5 * iqr
-    upper_limit = q3 +1.5 * iqr
+    upper_limit = q3 + 1.5 * iqr
 
-    df[column] = df[(df[column] >= lower) and (df[column]<=upper)]
-    return df 
+    df[column] = df[(df[column] >= lower) and (df[column] <= upper)]
+    return df
 
 
 '''
 Feature Selections
         remove unnecessary or irrelevant columns
 '''
-def select_features(df,selected_columns):
-    return[selected_columns]
 
+
+def select_features(df, selected_columns):
+    return [selected_columns]
 
 
 '''
 Feature Transformation
     creates a new columns or modifies one
 '''
+
+
 def create_features(df):
     if "salary" in df.columns:
-        def["salary_log"] = df["salary"].apply(lambda x: if x >=0 else 0)
+        def ["salary_log"] = df["salary"].apply(lambda x: if x >= 0 else 0)
 
 
 '''
@@ -139,16 +150,18 @@ FINAL COMPONENT
 '''
 
 
-
 def cleaning_changng_pipeline(file_path):
-   df = load_file(file_path)
-   df = validate_pipeline(df,['age','name','salary']) '''for coln in colns: if coln not in colns: raise ValueError()   '''
-   df = handle_missing_values(df)
-   ''' for coln in df.select_datatype(include = "numbers").columns:
+    df = load_file(file_path)
+    df = validate_pipeline(df, ['age', 'name', 'salary']) '''for coln in colns: if coln not in colns: raise ValueError()   '''
+    df = handle_missing_values(df)
+    ''' for coln in df.select_datatype(include = "numbers").columns:
             df[coln].fillna(df[coln].median(),inplace = True)
     '''
-    df = remove_duplicates(df) 
-    df = standardize_datatype(df) 
-    df = cleaning_categorical_data()
+    df = remove_duplicates(df) ''' df.drop_duplicated(inplace =True)'''
+    df = standardize_datatype(df) ''' .astype(int) '''
+    df = cleaning_categorical_data() ''' df['gender'].replace({ 'male':'m'}) '''
+    df = handle_outliers(df, column)
+    df = create_features(df)
+    df = select_features(df, ['age', 'salary', 'gender'])
 
-   
+    return df
